e49abb758fb08b4fe48cb8ca36460bbe375b3750
Jeff <jzinger100@gmail.com>
2021-05-01 16:31:35 +0200

    Lesson 1 - exercise 1 & 2

---
+++ src/Heading.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <header className="App-header">
+      <img
+        className="App-logo"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        alt="logo"
+      />
+      <h1>CYF Hotel</h1>
+    </header>
+  );
+}
+
+export default Heading;
